475b19402e7ebaa1d4e839338f70f8a45e022437
contrerasorihuelabenjamin <contrerasorihuelabenjamin@gmail.com>
2026-06-07 18:00:46 -0600

    Add files via upload

---
+++ InstruccionWhile.java
@@ -0,0 +1,12 @@
+public class InstruccionWhile {
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        while (numero <= 10) {
+            System.out.println("Texto " + numero);
+            numero = numero + 1;
+        }
+
+    } 
+}
